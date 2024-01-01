deb502368352a9f242e43c61dcd557e23804ab3a
Mohammad Qudah <qudahmohammad2002@gmail.com>
2023-12-31 21:28:54 -0500

    Comments for Todo File

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import React from 'react' // Import from react library to create React components
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' // This is used for rendering Font Awesome icons in the React application
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons' // This is the pen to squre icon used to let the user know where to edit the task
+import { faTrash } from '@fortawesome/free-solid-svg-icons' // This is the trash/delete icon used to delete the task by the user
 
-export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
+export const Todo = (props) => { /* React Functional component (Todo) that takes a single parameter (props). 
+                                    The parameter is an object that contains the properties passed to the component */
+    const { task, toggleComplete, deleteTodo, editTodo } = props; // This is Object destructuring, used to extract specific properties from props (task, editTodo, ...)
+    //JSX Structure
     return (
-        <div className='Todo'>
-            <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div className='Todo'> 
+            <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p> 
             <div>
                 <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
                 <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -10,14 +10,15 @@ export const TodoForm = ({ addTodo }) => {
 
         setValue("")
     }
-    
+
     return (
         <form className='TodoForm' onSubmit={handleSubmit}>
             <input type="text" className='todo-input'
                 value={value} placeholder='What is the task today?'
                 onChange={(e) => setValue(e.target.value)} />
             <button type='submit' className='todo-btn'>
-                Add Task</button>
+                Add Task
+            </button>
         </form>
     )
 }
