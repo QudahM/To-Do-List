5799e06a97100c45d5992e6ad9b9339519d8c643
Mohammad Qudah <qudahmohammad2002@gmail.com>
2025-01-01 03:55:58 -0500

    Reorganizing
    Reorganized my code, placed the code for the database in its own js file.
    Fixed my .gitignore to include all the new updates for the backend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
 const todoRoutes = require('./routes/todoRoutes');
+const connectDB = require('./config/db');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error(err));
+connectDB();
 
 app.use('/api/todos', todoRoutes);
 
